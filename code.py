941bc6b61aa24a894f3db203a4733c1992c0fcb6
Ilya Koliev <77661703+ilyakoliev@users.noreply.github.com>
2021-03-15 01:34:18 +0300

    Update code.py

--- code.py
+++ code.py
@@ -5,40 +5,32 @@ path = 'PATH_TO_CHROMEDRIVER'
 likes = int(input('How many likes? '))
 post = input('Link to the post: ')
 
+def check():
+    try:
+        driver.find_element_by_class_name('_subscription__button_5baba').click()
+    except:
+        print('Email request not found')
+
 for i in range(int(likes / 5)):
     driver = Chrome(executable_path=path)
     time.sleep(2)
     driver.get(post)
     time.sleep(2)
-    driver.execute_script("window.scrollTo(0, 5000);")
-    driver.execute_script("window.scrollTo(0, -500);")
-    time.sleep(2)
-    try:
-        driver.find_element_by_class_name('_subscription__button_5baba').click()
-    except:
-        print('Email request not found')
-    for i in range(5):
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    element = driver.find_element_by_class_name('_inner_8b2d8')
+    element.location_once_scrolled_into_view
+    check()
     for i in range(5):
         driver.find_element_by_class_name('_inner_8b2d8').click()
-    time.sleep(2)
     driver.close()
-    time.sleep(2)
+    time.sleep(1)
 
 driver = Chrome(executable_path=path)
 time.sleep(2)
 driver.get(post)
 time.sleep(2)
-driver.execute_script("window.scrollTo(0, 5000);")
-driver.execute_script("window.scrollTo(0, -500);")
-time.sleep(2)
-try:
-    driver.find_element_by_class_name('_subscription__button_5baba').click()
-except:
-    print('Email request not found')
-for i in range(5):
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+element = driver.find_element_by_class_name('_inner_8b2d8')
+element.location_once_scrolled_into_view
+check()
 for i in range(likes % 5):
     driver.find_element_by_class_name('_inner_8b2d8').click()
-time.sleep(2)
 driver.close()
